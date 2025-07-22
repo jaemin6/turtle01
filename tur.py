971a19a70d6c9cdbef7b2f6add0485561d899e25
jaemin6 <gumt555@gmail.com>
2025-07-22 10:19:54 +0900

    장애물 설치
    장애물 설치

--- tur.py
+++ tur.py
@@ -5,12 +5,22 @@ s = turtle.getscreen()
 # 거북이 변수 지정
 t = turtle.Turtle()
 t.shape("turtle")
-t.color("green")
+t.color("sky blue")
 
-# 왼쪽 아래(-300, -300)로 이동
+t.penup()
+t.goto(-50, 0)  # 네모 시작 위치
+t.pendown()
+t.fillcolor("red")
+t.begin_fill()
+for _ in range(4):
+    t.forward(60)  # 변 길이
+    t.left(90)
+t.end_fill()
+
+# 왼쪽 아래 (-300, -300)로 이동 (출발점)
 t.penup()
 t.goto(-300, -300)
 t.pendown()
 
-# 오른쪽 위(300, 300)로 이동
+# 오른쪽 위(300, 300)로 이동 (목적지)
 t.goto(300, 300)

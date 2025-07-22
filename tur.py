c6c13c92517d9f4c2d668feabfe1a38f4b8c33a1
jaemin6 <gumt555@gmail.com>
2025-07-22 10:59:59 +0900

    목적지 구현
    목적지 구현

--- tur.py
+++ tur.py
@@ -17,13 +17,31 @@ for _ in range(4):
     t.left(90)  # 사각형은 90
 t.end_fill()
 
+# 목적지점 만들기
+t.penup()
+t.goto(280, 280)  # 목적지점 좌표 거북이가 들어갈 수 있게
+t.setheading(0)   # 방향 초기화 (오른쪽)
+
+t.forward(10)    
+t.right(90)
+t.forward(10)
+t.left(90)
+
+t.pendown()
+t.fillcolor("black")
+t.begin_fill()
+for _ in range(4):
+    t.forward(60)
+    t.left(90)
+t.end_fill()
+
 # 왼쪽 아래 (-300, -300)로 이동 (출발점)
 t.penup()
 t.goto(-300, -300)
 t.pendown()
 
 # 오른쪽 위(300, 300)로 이동 (목적지)
-t.goto(-60, 10)
+t.goto(-60, 10)  # 장애물 도착 전 좌표
 # 장애물 회피
 t.goto(-100, 80)
 # 회피 후 목적지 도착

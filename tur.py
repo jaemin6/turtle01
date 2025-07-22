647293d9a4757d5865445282e92bab03d7612bf5
jaemin6 <gumt555@gmail.com>
2025-07-22 10:11:39 +0900

    목적지 도착
    거북이 목적지 도착

--- tur.py
+++ tur.py
@@ -4,3 +4,13 @@ import turtle
 s = turtle.getscreen()
 # 거북이 변수 지정
 t = turtle.Turtle()
+t.shape("turtle")
+t.color("green")
+
+# 왼쪽 아래(-300, -300)로 이동
+t.penup()
+t.goto(-300, -300)
+t.pendown()
+
+# 오른쪽 위(300, 300)로 이동
+t.goto(300, 300)

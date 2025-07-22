da335fdb6dcda4cbc88ed4bcf7ad4a20e6b75250
jaemin6 <gumt555@gmail.com>
2025-07-22 10:47:40 +0900

    장애물 회피
    장애물 회피

--- tur.py
+++ tur.py
@@ -8,13 +8,13 @@ t.shape("turtle")
 t.color("sky blue")
 
 t.penup()
-t.goto(-50, 0)  # 네모 시작 위치
+t.goto(-50, 0)  # 장애물 시작 위치 (화면 가운데)
 t.pendown()
 t.fillcolor("red")
 t.begin_fill()
 for _ in range(4):
-    t.forward(60)  # 변 길이
-    t.left(90)
+    t.forward(60)  
+    t.left(90)  # 사각형은 90
 t.end_fill()
 
 # 왼쪽 아래 (-300, -300)로 이동 (출발점)
@@ -23,4 +23,6 @@ t.goto(-300, -300)
 t.pendown()
 
 # 오른쪽 위(300, 300)로 이동 (목적지)
-t.goto(300, 300)
+t.goto(-60, 10)
+# 장애물 회피
+t.goto(-100, 80)
